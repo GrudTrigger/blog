266d4b3aa737bafebabb3c4fe5caa1b12f951cad
George <g.murashov@inbox.ru>
2025-02-16 15:18:01 +0300

    conf&handler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"backend/blog/configs"
+	"backend/blog/internal/auth"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	config := configs.LoadConfig()
 	router := http.NewServeMux()
+
+	//Repository
+
+	//Services
+	authService := auth.NewAuthService()
+
+	//Handlers
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		AuthService: authService,
+		Configs:     config,
+	})
 
 	server := http.Server{
 		Addr:    "8081",
--- server/internal/auth/handler.go
+++ server/internal/auth/handler.go
@@ -1,1 +1,37 @@
 package auth
+
+import (
+	"backend/blog/configs"
+	"net/http"
+)
+
+type AuthHandler struct {
+	*AuthService
+	config *configs.Configs
+}
+
+type AuthHandlerDeps struct {
+	*AuthService
+	*configs.Configs
+}
+
+func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
+	handler := &AuthHandler{
+		config:      deps.Configs,
+		AuthService: deps.AuthService,
+	}
+	router.HandleFunc("POST /auth/login", handler.Login())
+	router.HandleFunc("POST /auth/register", handler.Register())
+}
+
+func (handler *AuthHandler) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (handler *AuthHandler) Register() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
